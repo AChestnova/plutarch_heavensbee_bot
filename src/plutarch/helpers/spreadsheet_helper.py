a0e99a8431d64c0675c022faefc1e93b9cdb64be
Konstantin Chestnov <chestnovk@gmail.com>
2025-03-01 22:09:10 +0100

    Placeholder for interactions with Google Sheets

--- src/plutarch/helpers/spreadsheet_helper.py
+++ src/plutarch/helpers/spreadsheet_helper.py
@@ -1,6 +1,42 @@
 """
 Set of functions to manipulate Goodgle spreadsheet
 """
+from dataclasses import dataclass
+from datetime import time
+
+
+@dataclass
+class Player:
+    user_name: str
+    name: str
+    balance: int
+    can_sell: bool
+    prio: int
+
+@dataclass
+class Game:
+    game_date: time
+    capacity: int
+    price_per_player: int
+    is_summarized: bool
+
+@dataclass
+class Registration:
+    user_name: str
+    game_date: time
+    requested_at: time
+    prio: int
+
+@dataclass
+class AvailableSlot:
+    game_date: time
+    requested_at: time
+    user_name: str
+    prio: int
+
+
+class Database():
+    pass
 
 class SpreadsheetHelper:
     """Set of functions to manipulate Goodgle spreadsheet"""
